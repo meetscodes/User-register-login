d8713e9c795376c0b402ad7632cd2f4dce2c76ba
ravi italiya <italiyaravi704@gmail.com>
2024-04-25 20:18:10 +0530

    add jwt verification

--- server.js
+++ server.js
@@ -15,6 +15,36 @@ app.post("/login", (req, res) => {
   });
 });
 
+const verifyToken = (req, res, next) => {
+  const bearerHeader = req.headers["authentication"];
+
+  if (typeof bearerHeader !== "undefined") {
+    const bearer = bearerHeader.split(" ");
+    const token = bearer[1];
+    req.token = token;
+
+    next();
+  } else {
+    res.send({
+      result: "token is not valid",
+    });
+  }
+};
+
+app.post("/profile", verifyToken, (req, res) => {
+  jwt.verify(req.token, secretkey, (err, authData) => {
+    if (err) {
+      res.send({
+        result: "token is not valid",
+      });
+    } else {
+      res.json({
+        authData,
+      });
+    }
+  });
+});
+
 
 
 
