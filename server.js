3006c2e6fa8deb3b7e453cae762a5c438a6d0ec2
ravi italiya <italiyaravi704@gmail.com>
2024-04-25 20:17:23 +0530

    login route setup

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ const secretkey = "niwhefhwefhwoeoqdjoqj";
 
 const app = express();
 
+app.post("/login", (req, res) => {
+  const userdata = req.body;
+
+  jwt.sign({ userdata }, secretkey, { expiresIn: "7d" }, (err, token) => {
+    res.json({
+      token,
+    });
+  });
+});
+
+
+
 
 app.listen(3000, () => {
   console.log("server running on port 3000");
