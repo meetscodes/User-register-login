5bd38e302e876c38ccfa96ebb5857d319a1fea86
ravi italiya <italiyaravi704@gmail.com>
2024-04-26 15:20:25 +0530

    user schema created

--- src/user/userController.js
+++ src/user/userController.js
@@ -1,60 +1,75 @@
-import express from "express";
 const secretkey = "niwhefhwefhwoeoqdjoqj";
+import userModel from "./userModel";
+import jwt from "jsonwebtoken";
 
-const app = express();
+const register = async (req, res) => {
+  const { username, password } = req.body;
 
+  if (!username || !password) {
+    res.status(401).json({ error: "all filds are require" });
+  }
 
-const login = async (req,res)=> {
-        const { username, password } = req.body; 
-      
-        jwt.sign({ username, password }, secretkey, { expiresIn: "7d" }, (err, token) => {
-          if (err) {
-            res.status(500).json({ error: 'Failed to generate token' });
-          } else {
-            res.json({
-              token,
-            });
-          }
+  try {
+    const isUserExist = await userModel.findOne({ username });
+
+    if (isUserExist) {
+      res.status(401).json({ error: "user is allredy exist" });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  jwt.sign(
+    { username, password },
+    secretkey,
+    { expiresIn: "7d" },
+    (err, token) => {
+      if (err) {
+        res.status(500).json({ error: "Failed to generate token" });
+      } else {
+        res.json({
+          token,
         });
       }
+    }
+  );
+};
 
 const verifyToken = (req, res, next) => {
-    const bearerHeader = req.headers["authorization"]
-  
-    if (typeof bearerHeader !== "undefined") {
-      const bearer = bearerHeader.split(" ");
-      const token = bearer[1];
-      req.token = token;
-  
-      jwt.verify(token, secretkey, (err, data) =>{
-        if(err){
-          res.status(401).send({
-            result: "token is not valid",
-          });
-        }else{
-          req.user = data;
-          // chack in
-          next()
-        }
-      })
-    } else {
-      res.status(400).send({
-        result: "token is not found",
-      });
-    }
-  };
+  const bearerHeader = req.headers["authorization"];
 
+  if (typeof bearerHeader !== "undefined") {
+    const bearer = bearerHeader.split(" ");
+    const token = bearer[1];
+    req.token = token;
 
-  const post = (req, res) =>{
-    res.send(new Array(50))
-
+    jwt.verify(token, secretkey, (err, data) => {
+      if (err) {
+        res.status(401).send({
+          result: "token is not valid",
+        });
+      } else {
+        req.user = data;
+        // chack in
+        next();
+      }
+    });
+  } else {
+    res.status(400).send({
+      result: "token is not found",
+    });
   }
-  const profile = (req, res) =>{
-    res.json(req.user)
-
-  }
-
-
-export {
-  login,verifyToken,profile,post
 };
+
+const post = (req, res) => {
+  res.send(new Array(50));
+};
+const profile = (req, res) => {
+  res.json(req.user);
+};
+
+export { login, verifyToken, profile, post, register };
---
+++ src/user/userModel.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.Schema(
+    {
+        username:{
+            type: String,
+            require: true,
+        },
+        password:{
+            type: String,
+            require: true,
+        }
+    },
+    { timestamps: true }
+)
+
+export default mongoose.model("user",userSchema);
